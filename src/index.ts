05ed4bff925f0a632c8bb7e8f39d2a4daef9b751
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-10-15 13:30:45 +0300

    Prettier fix

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express from "express";
 import { Request, Response } from "express";
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 
 const port = Number(process.env.SERVER_PORT);
-const hostname: string = process.env.SERVER_IP as string
+const hostname: string = process.env.SERVER_IP as string;
 
 app.get("/", (req: Request, res: Response) => {
-	res.send({ data: "Hello World!" });
-})
+  res.send({ data: "Hello World!" });
+});
 
 app.listen(port, hostname, () => {
-	console.clear()
-	console.log(`Server started at: http://${hostname}:${port}`)
-})
+  console.clear();
+  console.log(`Server started at: http://${hostname}:${port}`);
+});
