be1509fdafe2921b8783cee7685dfdf25d0c5c81
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-10-28 13:10:25 +0300

    configured swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
-const User = require('./Models/User')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const app = express()
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve);
+app.use('/api-docs',swaggerUi.setup(swaggerDocument))
 
 require('./Controllers/AuthController')(app)
 require('./Controllers/DeviceController')(app)
