03329b49aac2346ab2a097ca5aee5682105d2283
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-10-28 10:29:50 +0300

    Added Device controller with GET and POST request

---
+++ src/Controllers/AuthController.js
@@ -0,0 +1,11 @@
+module.exports = (app)=>{
+	// Register
+	app.post("/register", (req, res) => {
+		// our register logic goes here...
+	});
+
+	// Login
+	app.post("/login", (req, res) => {
+		// our login logic goes here
+	});
+}
---
+++ src/Controllers/DeviceController.js
@@ -0,0 +1,27 @@
+const { Types } = require('mongoose');
+const Device = require('../Models/Device')
+
+module.exports = (app)=>{
+	//Get all devices
+	app.get('/device',(req,res)=>{
+		Device.find({},(err,devices)=>{
+			if(err) {
+				console.error(err);
+				res.sendStatus(400);
+				return;
+			}
+			res.send(devices);
+		})
+	})
+
+	app.post('/device',(req,res)=>{
+		if(!req.body) return res.sendStatus(400);
+		const serialNumber = req.body.serialNumber;
+		const newDevice = new Device({_id:Types.ObjectId(), serialNumber});
+		newDevice.save((err)=>{
+			if(err) return console.error(err);
+        res.send(newDevice);
+		})
+	})
+
+}
