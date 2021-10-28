58b8d5b521fde2ee43cb163065e896f1446feb6b
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-10-28 11:46:41 +0300

    Added GET PUT and POST requests for Device

--- src/Controllers/DeviceController.js
+++ src/Controllers/DeviceController.js
@@ -1,4 +1,4 @@
-const { Types } = require('mongoose')
+const mongoose = require('mongoose')
 const Device = require('../Models/Device')
 
 module.exports = (app) => {
@@ -17,10 +17,52 @@ module.exports = (app) => {
   app.post('/device', (req, res) => {
     if (!req.body) return res.sendStatus(400)
     const serialNumber = req.body.serialNumber
-    const newDevice = new Device({ _id: Types.ObjectId(), serialNumber })
-    newDevice.save((err) => {
-      if (err) return console.error(err)
-      res.send(newDevice)
+    try{
+      const newDevice = new Device({
+        _id: mongoose.Types.ObjectId(),
+        serialNumber: serialNumber
+      })
+  
+      newDevice.save((err) => {
+        if (err) return console.error(err)
+        res.send(newDevice)
+      })
+    }catch(err){
+      console.log(err)
+    }
+  })
+
+  app.put('/device/:id',(req,res)=>{
+    if (!req.body) return res.sendStatus(400)
+    const id = req.params.id;
+    const serialNumber = req.body.serialNumber;
+
+    try{
+      const newDevice = new Device({
+        _id:id,
+        serialNumber: serialNumber
+      })
+
+      Device.findOneAndUpdate({_id:id},newDevice, {new:true},(err,device)=>{
+        if(err) return console.log(err); 
+        res.send(newDevice);
+      })
+
+    }catch(err){
+      console.error(err);
+    }
+  })
+
+  app.get('/device/:id',(req,res)=>{
+
+    const id = req.params.id;
+    Device.findById(id, (err, device) => {
+      if (err) {
+        console.error(err)
+        res.sendStatus(400)
+        return
+      }
+      res.send(device)
     })
   })
 }
--- src/Models/Device.js
+++ src/Models/Device.js
@@ -2,7 +2,12 @@ const { model, Schema } = require('mongoose')
 
 const schema = new Schema({
   _id: Schema.Types.ObjectId,
-  serialNumber: String,
+  serialNumber: {
+    type:String,
+    unique:true,
+    required:true,
+    dropDups:true
+  },
   sensors: [{ type: Schema.Types.ObjectId, ref: 'Sensor' }]
 })
 const Device = model('Device', schema)
