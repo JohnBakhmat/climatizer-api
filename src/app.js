c1f9c4ecf4d71f1292adb88efbbcb9df698a80df
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-10-28 10:29:25 +0300

    Configured server start

---
+++ src/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require('mongoose')
+const User = require('./Models/User')
+const app = express();
+app.use(express.json());
+
+require('./Controllers/AuthController')(app);
+require('./Controllers/DeviceController')(app);
+
+// Logic goes here
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
+const server = http.createServer(app);
+
+const { API_PORT, MONGO_URI } = process.env;
+const port = process.env.PORT || API_PORT;
+
+// server listening
+mongoose.connect(MONGO_URI, (err) => {
+  err && console.error(err);
+  server.listen(port, () => {
+    console.clear();
+    console.log(`Server running on port ${port}`);
+  });
+});
