2a34dd10469d8aae13c6f77a1e19094fef346697
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-11-03 11:56:47 +0200

    Configured Auth

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -1,25 +1,64 @@
 const jwt = require('jsonwebtoken')
-const privateKey = '1a2b-3c4d-5e6f-7g8h'
+const Users = require('../Models/User')
+const {JWT_PRIVATE_KEY, JWT_PUBLIC_KEY} = process.env;
+const mongoose = require('mongoose')
+
 module.exports = (app) => {
   // Register
-  app.post('/register', (req, res) => {
+  app.post('/auth/login', (req, res) => {
     // our register logic goes here...
     const body = req.body
-    
-    jwt.sign(body, privateKey, { algorithm: 'HS256' }, (error, token) => {
-      if (error) res.status(500).send(error)
-      res.send({ idToken: token })
+    const config = { 
+      algorithm: 'HS256'
+    }
+    Users.findOne({email:body.email,password:body.password},(err,user)=>{
+      if(!user){
+        res.status(404).send("User wasn't found!");
+      }
+      else if(err){
+        res.status(500).send(err);
+      }
+      else{
+        jwt.sign(body, JWT_PRIVATE_KEY, config, (error, token) => {
+          if (error) res.status(500).send(error)
+          res.status(200).send({ idToken: token })
+        })
+      }
     })
+    
   })
 
   // Login
-  app.post('/login', (req, res) => {
+  app.post('/auth/register', (req, res) => {
     // our login logic goes here
     const body = req.body
-
-    jwt.sign(body, privateKey, { algorithm: 'HS256' }, (error, token) => {
-      if (error) res.status(500).send(error)
-      res.send({ idToken: token })
+    const config = { 
+      algorithm: 'HS256'
+    }
+    Users.findOne({email:body.email,password:body.password},(err,user)=>{
+      if(user){
+        res.status(200).send('User already exists');
+      }
+      else if(err){
+        res.status(500).send(err);
+      }
+      else{
+        const newUser = new Users({
+          _id: mongoose.Types.ObjectId(),
+          email: body.email,
+          password: body.password
+        })
+        newUser.save(error=>{
+          if(error){
+            res.status(500).send(error)
+          }
+        })
+        jwt.sign(body, JWT_PRIVATE_KEY, config, (error, token) => {
+          if (error) res.status(500).send(error)
+          res.send({ idToken: token })
+        })
+      }
     })
+    
   })
 }
