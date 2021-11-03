60ef632448a0e0dd0971a581b54bf0ce4065e64e
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-11-03 10:22:09 +0200

    Added AccessController

---
+++ src/Controllers/AccessController.js
@@ -0,0 +1,78 @@
+const { Types } = require('mongoose');
+const Access = require('../Models/Access');
+
+module.exports = (app)=>{
+	app.get('/access',(req,res)=>{
+		Access.find({},(err,rooms)=>{
+			if(err){
+				console.error(err);
+				res.sendStatus(400);
+				return;
+			}
+			res.send(rooms);
+		})
+	});
+	app.get('/access/:id',(req,res)=>{
+		const id = req.params.id;
+		Access.findById(id,(err,room)=>{
+			if(err){
+				console.error(err);
+				res.sendStatus(400);
+				return;
+			}
+			res.send(room);
+		})
+	});
+	app.post('/access',(req,res)=>{
+		if(!req.body) return res.sendStatus(400);
+		const body = req.body;
+		try{
+			const newAccess = new Access({
+				_id: Types.ObjectId(),
+				room: body.roomId,
+				user: body.userId,
+				isAllowed: true
+			})
+			newAccess.save(error=>{
+				throw error;
+			});
+		}catch(e){
+			console.error(e);
+			res.sendStatus(400);
+		}
+	});
+	app.put('/access/:id',(req,res)=>{
+		if (!req.body) return res.sendStatus(400)
+    const id = req.params.id;
+    const body = req.body;
+
+    try{
+      const newAccess = new Room({
+        _id:id,
+        room: body.roomId,
+				user: body.userId,
+				isAllowed: true
+      })
+
+      Access.findOneAndUpdate({_id:id},newAccess, {new:true},(err,room)=>{
+        if(err) return console.log(err); 
+        res.send(room);
+      })
+
+    }catch(err){
+      console.error(err);
+    }
+	});
+
+	app.delete('/access/:id',(req,res)=>{
+		const id = req.params.id;
+    	Access.findByIdAndDelete(id, (err, room) => {
+      if (err) {
+        console.error(err)
+        res.sendStatus(400)
+        return
+      }
+      res.send(room)
+    })
+	});
+}
