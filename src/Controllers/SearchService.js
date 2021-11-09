b99d8028726051a2a3ac705a914aa25ad832a2e4
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-11-09 14:33:29 +0200

    Renamed Controllers into Services

--- src/Controllers/SearchService.js
+++
@@ -1,4 +0,0 @@
-const { verifyJwtToken } = require('../Middleware/JwtService')
-module.exports = (app) => {
-  app.post('/search/', verifyJwtToken)
-}
--- src/app.js
+++ src/app.js
@@ -8,16 +8,16 @@ app.use(express.json())
 app.use('/api-docs', swaggerUi.serve)
 app.use('/api-docs', swaggerUi.setup(swaggerDocument))
 
-require('./Controllers/AuthController')(app)
+require('./Services/AuthService')(app)
 
-require('./Controllers/AccessController')(app)
-require('./Controllers/BuildingController')(app)
-require('./Controllers/ClimateSettingController')(app)
-require('./Controllers/DeviceController')(app)
-require('./Controllers/PresetController')(app)
-require('./Controllers/RoomController')(app)
-require('./Controllers/SensorController')(app)
-require('./Controllers/SensorDataController')(app)
+require('./Services/AccessService')(app)
+require('./Services/BuildingService')(app)
+require('./Services/ClimateSettingService')(app)
+require('./Services/DeviceService')(app)
+require('./Services/PresetService')(app)
+require('./Services/RoomService')(app)
+require('./Services/SensorService')(app)
+require('./Services/SensorDataService')(app)
 
 // Logic goes here
 
